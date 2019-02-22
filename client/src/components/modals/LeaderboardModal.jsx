bccba8ddb43fd0606fb7e016f5507706900d73f8
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-22 22:31:38 +0700

    [prog] work on leaderboard modal

--- client/src/components/modals/LeaderboardModal.jsx
+++ client/src/components/modals/LeaderboardModal.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LeaderboardModalCloseBtn from './LeaderboardModalCloseBtn';
 
-const LeaderboardModal = ({ allScores }) => (
+const LeaderboardModal = ({ allScores, closeLeaderboard }) => (
   <div className="leaderboardmodal">
     <div className="leaderboardmodalcontent">
-      {allScores.map(score => (
-        <div>Score</div>
-      ))}
+      <div className="leaderboardmodaltop">
+        <LeaderboardModalCloseBtn closeLeaderboard={closeLeaderboard} />
+      </div>
+      <h2 className="leaderboardheader"><span>High Scores</span></h2>
+      <div className="leaderboardmodallist">
+        {allScores.map((score, i) => (
+          <div className="leaderboardscore" key={i}>{score.score}</div>
+        ))}
+      </div>
     </div>
   </div>
 );
 
 LeaderboardModal.propTypes = {
   allScores: PropTypes.arrayOf(PropTypes.object).isRequired,
+  closeLeaderboard: PropTypes.func.isRequired,
 };
 
 export default LeaderboardModal;
