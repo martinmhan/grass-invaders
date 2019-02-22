ac1013c39f1d3e1ccd4cc0f3fc1c059b2358c89f
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-22 22:35:08 +0700

    [prog] work on leaderboard

--- client/src/components/grid/Grid.jsx
+++ client/src/components/grid/Grid.jsx
@@ -3,11 +3,15 @@ import PropTypes from 'prop-types';
 import Row from './Row';
 import IntroModal from '../modals/IntroModal';
 import GameOverModal from '../modals/GameOverModal';
+import LeaderboardModal from '../modals/LeaderboardModal';
 
 const Grid = props => (
-  <div className="grid" tabIndex="0" onKeyDown={props.handleKeyDown}>
+  <div role="grid" className="grid" tabIndex="0" onKeyDown={props.handleKeyDown}>
     { props.gameState === 'game over' ? <GameOverModal /> : null }
     { props.gameState === 'intro' ? <IntroModal letsPlay={props.letsPlay} /> : null }
+    { props.gameState === 'leaderboard'
+      ? <LeaderboardModal closeLeaderboard={props.closeLeaderboard} allScores={props.allScores} />
+      : null }
     { props.gridMatrix.map((rowArr, row) => (
       <Row
         cols={props.cols}
@@ -25,6 +29,8 @@ Grid.propTypes = {
   handleKeyDown: PropTypes.func.isRequired,
   gridMatrix: PropTypes.arrayOf(PropTypes.array).isRequired,
   gameState: PropTypes.string.isRequired,
+  allScores: PropTypes.arrayOf(PropTypes.any).isRequired,
+  closeLeaderboard: PropTypes.func.isRequired,
 };
 
 export default Grid;
