344cd4cc8d4b58e0eddbd5f3054a64183da54dbe
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-22 22:32:06 +0700

    [prog] Add leaderboard button

--- client/src/components/ButtonPad.jsx
+++ client/src/components/ButtonPad.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ButtonPad = ({ startGame, endGame, showLeaderboard }) => (
+const ButtonPad = ({ startGame, endGame, openLeaderboard }) => (
   <div className="buttonpad">
     <button type="button" className="startgamebtn" onClick={startGame}>Start Game</button>
     <button type="button" className="endgamebtn" onClick={endGame}>End Game</button>
-    <button type="button" className="leaderboardbtn" onClick={showLeaderboard}>High Scores</button>
+    <button type="button" className="leaderboardbtn" onClick={openLeaderboard}>High Scores</button>
   </div>
 );
 
 ButtonPad.propTypes = {
   startGame: PropTypes.func.isRequired,
   endGame: PropTypes.func.isRequired,
-  showLeaderboard: PropTypes.func.isRequired,
+  openLeaderboard: PropTypes.func.isRequired,
 };
 
 export default ButtonPad;
