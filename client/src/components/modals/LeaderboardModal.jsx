abfd7163ce34038c72a2aefc02490ff0547b8fd0
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-23 18:15:34 +0700

    [prog] render high scores in leaderboard modal

--- client/src/components/modals/LeaderboardModal.jsx
+++ client/src/components/modals/LeaderboardModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 import LeaderboardModalCloseBtn from './LeaderboardModalCloseBtn';
 
 const LeaderboardModal = ({ allScores, closeLeaderboard }) => (
@@ -10,8 +11,17 @@ const LeaderboardModal = ({ allScores, closeLeaderboard }) => (
       </div>
       <h2 className="leaderboardheader"><span>High Scores</span></h2>
       <div className="leaderboardmodallist">
+        <div className="leaderboardentry">
+          <div className="leaderboardscore"><strong>Score</strong></div>
+          <div className="leaderboardusername"><strong>Username</strong></div>
+          <div className="leaderboarddate"><strong>Date</strong></div>
+        </div>
         {allScores.map((score, i) => (
-          <div className="leaderboardscore" key={i}>{score.score}</div>
+          <div className="leaderboardentry">
+            <div className="leaderboardscore" key={i}>{score.score}</div>
+            <div className="leaderboardusername" key={i}>{score.username}</div>
+            <div className="leaderboarddate" key={i}>{format(score.score_date, 'MM-DD-YYYY')}</div>
+          </div>
         ))}
       </div>
     </div>
