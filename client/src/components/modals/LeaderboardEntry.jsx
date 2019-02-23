cd9483d058f884ad5cd99b920eecd689af445800
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-23 20:28:01 +0700

    [prog] style leaderboard modal

---
+++ client/src/components/modals/LeaderboardEntry.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { format } from 'date-fns';
+
+const LeaderboardEntry = ({ score }) => (
+  <div className="leaderboardentry">
+    <div className="leaderboardscore">{score.score}</div>
+    <div className="leaderboardusername">{score.username}</div>
+    <div className="leaderboarddate">{format(score.score_date, 'MM-DD-YYYY')}</div>
+  </div>
+);
+
+LeaderboardEntry.propTypes = {
+  score: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default LeaderboardEntry;
--- client/src/components/modals/LeaderboardModal.jsx
+++ client/src/components/modals/LeaderboardModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { format } from 'date-fns';
+import LeaderboardListHeader from './LeaderboardListHeader';
+import LeaderboardEntry from './LeaderboardEntry';
 import LeaderboardModalCloseBtn from './LeaderboardModalCloseBtn';
 
 const LeaderboardModal = ({ allScores, closeLeaderboard }) => (
@@ -10,19 +11,9 @@ const LeaderboardModal = ({ allScores, closeLeaderboard }) => (
         <LeaderboardModalCloseBtn closeLeaderboard={closeLeaderboard} />
       </div>
       <h2 className="leaderboardheader"><span>High Scores</span></h2>
-      <div className="leaderboardmodallist">
-        <div className="leaderboardentry">
-          <div className="leaderboardscore"><strong>Score</strong></div>
-          <div className="leaderboardusername"><strong>Username</strong></div>
-          <div className="leaderboarddate"><strong>Date</strong></div>
-        </div>
-        {allScores.map((score, i) => (
-          <div className="leaderboardentry">
-            <div className="leaderboardscore" key={i}>{score.score}</div>
-            <div className="leaderboardusername" key={i}>{score.username}</div>
-            <div className="leaderboarddate" key={i}>{format(score.score_date, 'MM-DD-YYYY')}</div>
-          </div>
-        ))}
+      <LeaderboardListHeader />
+      <div className="leaderboardlist">
+        {allScores.map((score, i) => (<LeaderboardEntry score={score} key={i} />))}
       </div>
     </div>
   </div>
