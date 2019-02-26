0ef781adeb45fbd0a71fa465c4b1c850e223a0e3
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-26 15:04:26 +0700

    [refactor]

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     test: {
       presets: ['@babel/preset-env', '@babel/preset-react'],
-      plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-transform-export-extensions'],
+      plugins: ['babel-plugin-transform-export-extensions', '@babel/plugin-proposal-class-properties'],
     },
   },
 };
