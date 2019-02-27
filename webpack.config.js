18526dd1896e793237d71dfe7864dcf757ac6f1a
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-27 10:39:50 +0700

    [refactor] reorg webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,1 @@
-const devConfig = require('./webpack.config.dev');
-const prodConfig = require('./webpack.config.prod');
-
-const dev = 'development';
-const prod = 'production';
-
-module.exports = env => (
-  env === dev ? devConfig
-  : env === prod ? prodConfig
-  : null
-);
+module.exports = env => require(`./webpack.${env.NODE_ENV}`);
