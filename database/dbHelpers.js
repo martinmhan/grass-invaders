9474dfb0fd457f7a28ee61c4714e063cbae549da
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2019-03-01 11:11:56 -0800

    [testing] add console logs to server db requests

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -1,6 +1,11 @@
 const pool = require('./index.js');
 
 module.exports = {
-  selectScores: () => pool.query('SELECT * FROM scores;'),
-  insertScore: (username, score) => pool.query(`INSERT INTO scores (username, score, score_date) VALUES ('${username}', ${score}, '${new Date().toISOString().slice(0, 10)}');`),
+  selectScores: () => {
+    console.log('selectScores');
+    return pool.query('SELECT * FROM scores;');
+  },
+  insertScore: (username, score) => {
+    return pool.query(`INSERT INTO scores (username, score, score_date) VALUES ('${username}', ${score}, '${new Date().toISOString().slice(0, 10)}');`)
+  },
 };
